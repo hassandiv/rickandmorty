e159c4a44a85c47c8ee62314e9d034965fb0e528
Hassan Hafez Work <workuser@Hassans-Mac-mini.local>
2022-04-08 09:57:07 +0100

    header and footer moved to layout component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { ApolloProvider } from '@apollo/client'
 import client from './apollo/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AppStore} from './store/StoreProvider'
-import Header from './components/Header'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Favourite from './pages/Favourite'
 import CharacterView from './pages/CharacterView'
 import NotFound from './pages/404'
-import Footer from './components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -18,16 +16,14 @@ function App() {
         <ApolloProvider client={client}>
             <AppStore>
                 <Router>
-                    <Header/>
-                        <Layout>
-                            <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/favourite" element={<Favourite />} />
-                                <Route path="/character/:id" element={<CharacterView />} />
-                                <Route path="*" element={<NotFound />} />
-                            </Routes>
-                        </Layout>
-                    <Footer/>
+                    <Layout>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/favourite" element={<Favourite />} />
+                            <Route path="/character/:id" element={<CharacterView />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </Layout>
                 </Router>
             </AppStore>
         </ApolloProvider>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 import styles from '../styles/Layout.module.css'
 
 const Layout= ({ children }) => {
     
     return(
-        <main className={styles.container}>
-            {children}
-        </main>
+        <React.Fragment>
+            <Header/>
+                <main className={styles.container}>
+                    {children}
+                </main>
+            <Footer/>
+        </React.Fragment>
     )
 }
 
